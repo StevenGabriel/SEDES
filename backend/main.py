0c5ed1df369c1522cef4b4777423a754d949efc5
StevenGCT <StevenClarosT@outlook.com>
2026-08-28 09:05:57 -0400

    primer commit

---
+++ backend/main.py
@@ -0,0 +1,9 @@
+print('Claros Tapia Steven Gabriel')
+
+from fastapi import FastAPI
+
+app = FastAPI(title="API SEDES Lab")
+
+@app.get("/")
+def leer_raiz():
+    return {"mensaje": "¡El backend del SEDES está funcionando perfectamente!"}
